cc4f63af921514932be34958b97d6d80b4cda00b
kiera <kieranmthompson@live.com>
2016-04-17 16:58:16 +1000

    yup

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -13,7 +13,22 @@ namespace ConsoleApplication1
             String message = "Hello World";
             Console.WriteLine(message);
             Console.WriteLine("Press Enter to close...");
+            Console.WriteLine("Or perhaps a change is in order... What do you think? Yes or No?");
+            string response = Console.ReadLine();
+            if (response == "Yes")
+                {
+                Console.WriteLine("A change has been made.");
+            }
+            else if (response == "No")
+            {
+                Console.WriteLine("You were wise.");
+            }
+            else
+            {
+                Console.WriteLine("Foolish child");
+            }
             Console.ReadLine();
+
         }
     }
 }
